324839519c1b7130aa9dde7a8bbd3bff29b7c853
Chris Klosowski <cklosowski@gmail.com>
2016-02-05 07:06:48 -0700

    Use ppp_manage_role filter for all settings screens #149

--- post-promoter-pro.php
+++ post-promoter-pro.php
@@ -180,44 +180,51 @@ class PostPromoterPro {
 	 * @access public
 	 */
 	public function ppp_setup_admin_menu() {
-		add_menu_page( __( 'Post Promoter', 'ppp-txt' ),
-		               __( 'Post Promoter', 'ppp-txt' ),
-		               apply_filters( 'ppp_manage_role', 'administrator' ),
-		               'ppp-options',
-		               'ppp_admin_page'
-		             );
+		$role = apply_filters( 'ppp_manage_role', 'administator' );
 
-		add_submenu_page( 'ppp-options',
-			              __( 'Social Settings', 'ppp-txt' ),
-			              __( 'Social Settings', 'ppp-txt' ),
-			              'manage_options',
-			              'ppp-social-settings',
-			              'ppp_display_social'
-			            );
+		add_menu_page(
+			__( 'Post Promoter', 'ppp-txt' ),
+			__( 'Post Promoter', 'ppp-txt' ),
+			$role,
+			'ppp-options',
+			'ppp_admin_page'
+		);
 
-		add_submenu_page( 'ppp-options',
-			              __( 'Schedule', 'ppp-txt' ),
-			              __( 'Schedule', 'ppp-txt' ),
-			              'manage_options',
-			              'ppp-schedule-info',
-			              'ppp_display_schedule'
-			            );
+		add_submenu_page(
+			'ppp-options',
+			__( 'Social Settings', 'ppp-txt' ),
+			__( 'Social Settings', 'ppp-txt' ),
+			$role,
+			'ppp-social-settings',
+			'ppp_display_social'
+		);
 
-		add_submenu_page( 'ppp-options',
-			              __( 'System Info', 'ppp-txt' ),
-			              __( 'System Info', 'ppp-txt' ),
-			              'manage_options',
-			              'ppp-system-info',
-			              'ppp_display_sysinfo'
-			            );
+		add_submenu_page(
+			'ppp-options',
+			__( 'Schedule', 'ppp-txt' ),
+			__( 'Schedule', 'ppp-txt' ),
+			$role,
+			'ppp-schedule-info',
+			'ppp_display_schedule'
+		);
 
-		add_submenu_page( null,
-			              __( 'PPP Upgrades', 'ppp-txt' ),
-			              __( 'PPP Upgrades', 'ppp-txt' ),
-			              'manage_options',
-			              'ppp-upgrades',
-			              'ppp_upgrades_screen'
-			            );
+		add_submenu_page(
+			'ppp-options',
+			__( 'System Info', 'ppp-txt' ),
+			__( 'System Info', 'ppp-txt' ),
+			$role,
+			'ppp-system-info',
+			'ppp_display_sysinfo'
+		);
+
+		add_submenu_page(
+			null,
+			__( 'PPP Upgrades', 'ppp-txt' ),
+			__( 'PPP Upgrades', 'ppp-txt' ),
+			$role,
+			'ppp-upgrades',
+			'ppp_upgrades_screen'
+		);
 
 	}
 
