ac4f25a319e61e90c44f0b4e70a6385d49bfa445
Chris Klosowski <cklosowski@gmail.com>
2016-05-17 23:06:54 -0700

    Initial work for Pinterest #107

--- includes/libs/social-loader.php
+++ includes/libs/social-loader.php
@@ -5,10 +5,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-include_once( PPP_PATH . '/includes/wpme-functions.php' );
+require_once( PPP_PATH . '/includes/wpme-functions.php' );
 
 global $ppp_twitter_oauth;
-include_once( PPP_PATH . '/includes/twitter-functions.php' );
+require_once( PPP_PATH . '/includes/twitter-functions.php' );
 require_once( PPP_PATH . '/includes/libs/twitter.php');
 $ppp_twitter_oauth = new PPP_Twitter();
 
@@ -16,12 +16,12 @@ $ppp_twitter_oauth = new PPP_Twitter();
 require_once( PPP_PATH . '/includes/libs/twitter-user.php' );
 
 global $ppp_facebook_oauth;
-include_once( PPP_PATH . '/includes/facebook-functions.php' );
+require_once( PPP_PATH . '/includes/facebook-functions.php' );
 require_once( PPP_PATH . '/includes/libs/facebook.php');
 $ppp_facebook_oauth = new PPP_Facebook();
 
 global $ppp_bitly_oauth;
-include_once( PPP_PATH . '/includes/bitly-functions.php' );
+require_once( PPP_PATH . '/includes/bitly-functions.php' );
 require_once( PPP_PATH . '/includes/libs/bitly.php' );
 $ppp_bitly_oauth = new PPP_Bitly();
 
@@ -29,3 +29,8 @@ global $ppp_linkedin_oauth;
 include_once( PPP_PATH . '/includes/linkedin-functions.php' );
 require_once( PPP_PATH . '/includes/libs/linkedin.php' );
 $ppp_linkedin_oauth = new PPP_Linkedin();
+
+global $ppp_pinterest_oauth;
+require_once( PPP_PATH . '/includes/pinterest-functions.php' );
+require_once( PPP_PATH . '/includes/libs/Pinterest.php' );
+$ppp_pinterest_oauth = new PPP_Pinterest();
