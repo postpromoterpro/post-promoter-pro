f41e5f89caeab8d1d85f69faf879a364bc2570a0
Chris Klosowski <cklosowski@gmail.com>
2018-03-06 00:26:44 -0700

    Update to the newer filter name and arguments suggested in editflow #265

--- includes/integrations/edit-flow.php
+++ includes/integrations/edit-flow.php
@@ -11,10 +11,14 @@ function ppp_add_shares_calendar_data( $items, $week_single_date ) {
 }
 add_filter( 'ef_calendar_posts_for_week', 'ppp_add_shares_calendar_data', 10, 2 );
 
-function ppp_add_shares_to_ef_calendar( $output, $num, $post ) {
+function ppp_add_shares_to_ef_calendar( $output, $calendar_object, $num, $post, $week_single_date ) {
+	if ( ! is_array( $post ) ) {
+		return $output;
+	}
+
 	if ( $post['type'] === 'share' ) {
 		$output = $post['content'];
 	}
 	return $output;
 }
-add_filter( 'ef_calendar_single_date_item', 'ppp_add_shares_to_ef_calendar', 10, 3 );
+add_filter( 'pre_ef_calendar_single_date_item_html', 'ppp_add_shares_to_ef_calendar', 10, 5 );
