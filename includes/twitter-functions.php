33377a8090a9447c5e84f4ff2b62ab817e25ea62
Chris Klosowski <cklosowski@gmail.com>
2018-03-06 00:44:00 -0700

    Add alt text to the Twitter card images #282

--- includes/twitter-functions.php
+++ includes/twitter-functions.php
@@ -783,6 +783,14 @@ function ppp_tw_default_meta_elements() {
 	if ( $image_url ) {
 		$elements['twitter:card']      = 'summary_large_image';
 		$elements['twitter:image:src'] = $image_url;
+
+		$thumb_id = get_post_thumbnail_id( $post->ID );
+		$alt_text = get_post_meta( $thumb_id, '_wp_attachment_image_alt', true );
+
+		// When adding media via the WP Uploader, any 'alt text' supplied will be used as the accessible alt text.
+		if ( ! empty( $alt_text ) ) {
+			$elements['twitter:image:alt'] = esc_attr( $alt_text );
+		}
 	} else {
 		$elements['twitter:card'] = 'summary';
 	}
